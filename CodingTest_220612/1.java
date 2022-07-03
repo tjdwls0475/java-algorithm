4a7c7a7eb73a16574b26426eda2bad27f2ef8362
tjdwls0475 <tjdwls0475@hanmail.net>
2022-07-03 20:47:26 +0900

    Using Selection-Sort algorithm

--- CodingTest_220612/1.java
+++ CodingTest_220612/1.java
@@ -1,49 +1,36 @@
 import java.util.*;
 
-public class test {
+public class Solution {
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		int n = input.nextInt();
-		int[] numbers = new int[n];
-		
-		for (int i=0; i<numbers.length; i++) {
-			numbers[i] = input.nextInt();
-		}
-		input.close();
-		//System.out.println(Arrays.toString(numbers));
+		int[] numbers = {1, 3, 5, 6, 2};
+				
 		Solution T = new Solution();
-		System.out.println(T.solution(numbers));
+		System.out.println(Arrays.toString(T.solution(numbers)));
 	}
-}
 
-class Solution {
     public int[] solution(int[] p) {
-        int[] answer = new int[p.length];
-        int swap = 0;
-        int min = 0;
-        int pt = 0;
-        
-        for (int i=0; i<p.length; i++) {
-        	min = p[i];
-        	pt = 0;
-        	for (int j=i+1; j<p.length; j++) {
-        		
-        		if (min > p[j]) {
-        			min = p[j];
-        			pt = j;
-        		}
-        	}
+    	int[] answer = new int[p.length];
 
-        	if (pt >= 1) {
-        		swap = p[i];
-        		p[i] = p[pt];
-        		p[pt] = swap;
-        		
-        		answer[i]++;
-        		answer[pt]++;
-        	}
-        }
-       
+    	for (int i=0; i<p.length; i++) {
+    		int idx = i;
+    		int tmp = 0;
+    		
+    		for (int j=i+1; j<p.length; j++) {
+    			if (p[idx] > p[j]) {
+    				idx = j;
+    			}
+    		}
+    		
+    		if (idx != i) {
+    			answer[idx]++;
+    			answer[i]++;
+    			
+    			tmp = p[i];
+    			p[i] = p[idx];
+    			p[idx] = tmp;
+    		}
+    	}	
+ 
         return answer;
     }
 }
