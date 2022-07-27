098443c1338b84e2a49bce247c81de72f0dea996
tjdwls0475 <tjdwls0475@hanmail.net>
2022-07-27 10:29:54 +0900

    Update 1.java

--- inflearn/java-coding-test/7. recursive, tree, graph/1.java
+++ inflearn/java-coding-test/7. recursive, tree, graph/1.java
@@ -1,10 +1,10 @@
 import java.util.*;
 
 class Solution {
-    public void recursive(int n) {
+    public void DFS(int n) {
         if (n == 0) return;
         
-        recursive(n-1);
+        DFS(n-1);
         System.out.print(n);
         return;
     }
@@ -13,6 +13,6 @@ class Solution {
     public static void main(String[] args) {
     	
     	Solution T = new Solution();
-    	T.recursive(5);
+    	T.DFS(5);
     }
 }
