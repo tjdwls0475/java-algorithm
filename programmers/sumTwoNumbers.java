86834e5ee61c5de1cbc4bea963a35a9d2a9cd15c
tjdwls0475 <tjdwls0475@hanmail.net>
2022-07-28 13:22:15 +0900

    Create sumTwoNumbers.java

---
+++ programmers/sumTwoNumbers.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<Integer> solution(int[] numbers) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        HashSet<Integer> HS = new HashSet<>();
+        
+        for (int i=0; i<numbers.length-1; i++) {
+            for (int j=i+1; j<numbers.length; j++) {
+                HS.add(numbers[i] + numbers[j]);
+            }
+        }
+        
+        Iterator iter = HS.iterator();
+      
+        while(iter.hasNext()) {
+        	answer.add((int) iter.next());  // Only one per iter.next()
+        }
+      
+        Collections.sort(answer);
+      
+        return answer;
+    }
+    
+
+}
