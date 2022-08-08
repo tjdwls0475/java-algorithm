e758614f23b5d2bee986ae440e3963e4e2f5405b
tjdwls0475 <tjdwls0475@hanmail.net>
2022-08-08 19:56:11 +0900

    Create 3_wrong.java

---
+++ inflearn/java-coding-test/6. sorting and searching/3_wrong.java
@@ -0,0 +1,31 @@
+// Insertion Sort
+class Solution {
+	public static void main(String[] args) {
+		int[] numbers = {1, 4, 5, 2, 6, -2};
+		Solution T = new Solution();
+		System.out.println(T.sortNumbers(numbers)[0]);
+		System.out.println(T.sortNumbers(numbers)[1]);
+		System.out.println(T.sortNumbers(numbers)[2]);
+		System.out.println(T.sortNumbers(numbers)[3]);
+		System.out.println(T.sortNumbers(numbers)[4]);
+		System.out.println(T.sortNumbers(numbers)[5]);
+		System.out.println(T.sortNumbers(numbers)[0]);
+	}
+	
+	private int[] sortNumbers(int[] numbers) {
+		for(int i=1; i<numbers.length; i++) {
+			int tmp = numbers[i];
+			for(int j=i-1; j>=0; j--) {
+				if(numbers[j] > tmp) {
+					numbers[j+1] = numbers[j];
+				}
+				else {
+					numbers[j+1] = tmp; // When 'n'th array is the largest one, this would fail. 
+					break;
+				}
+			}
+		}
+		
+		return numbers;
+	}
+}
