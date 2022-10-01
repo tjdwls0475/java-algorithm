9fb77b17374b19179cac0b52ccc6c3ecc5ea8cc0
harry hwang <tjdwls0475@hanmail.net>
2022-10-01 18:55:06 +0900

    Create 1.java

---
+++ CodingTest_221001/2/1.java
@@ -0,0 +1,64 @@
+import java.util.Arrays;
+
+class Solution {
+    String[] registered_list;
+    String S;
+    int N;
+
+    public String solution(String[] registered_list, String new_id) {
+        this.registered_list = registered_list;
+        Arrays.sort(this.registered_list);
+        if(checkValidity(new_id)) return new_id;
+        
+        int idx = getLastStringIndex(new_id);
+        //System.out.print(idx);
+        
+        if(idx == new_id.length()) {
+            this.S = new_id;
+            this.N = 0;
+            return DFS2();
+        }
+        else {
+            this.S = new_id.substring(0, idx);
+            this.N = Integer.parseInt(new_id.substring(idx));
+            return DFS1();
+        }
+    }
+
+    private String DFS1() {
+        if(checkValidity(S + Integer.toString(N))) return S + Integer.toString(N);
+        else {
+            N++;
+            return DFS1();
+        }
+    }
+
+        private String DFS2() {
+        if(N==0) {
+            N++;
+            return DFS2();
+        }
+        else if(checkValidity(S + Integer.toString(N))) return S + Integer.toString(N);
+        else {
+            N++;
+            return DFS2();
+        }
+    }
+
+    private boolean checkValidity(String id) {
+        for(String list: registered_list) {
+            if(list.equals(id)) {
+                System.out.println("right");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private int getLastStringIndex(String s) {
+        for(int i=0; i<s.length(); i++) {
+            if(Character.isDigit(s.charAt(i))) return i;
+        }
+        return s.length();
+    }
+}
